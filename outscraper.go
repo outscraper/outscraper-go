981e0178a81fc1b0197983b781882fd6de82377a
vlad-outscraper <service@outscraper.com>
2022-07-25 16:20:09 +0300

    add endpoints

--- outscraper.go
+++ outscraper.go
@@ -1,9 +1,100 @@
-package main
+package outscraper
 
-import "fmt"
+import (
+    "fmt"
+    "net/url"
+    "net/http"
+    "encoding/json"
+    "errors"
+)
 
-// Hello returns a greeting for the named person.
-func main() {
-    // Return a greeting that embeds the name in a message.
-    fmt.Println("test")
+const (
+    ApiUrl string = "https://api.app.outscraper.com"
+)
+
+type Client struct {
+	apiKey string
+}
+
+type Request map[string]string
+
+type Result map[string]interface{}
+
+type ResultArray []interface{}
+
+func (c Client) getAPIRequest(path string, parameters map[string]string) (Result, error) {
+    httpClient := &http.Client{}
+    url, _ := url.Parse(ApiUrl + path)
+
+    query := url.Query()
+	if parameters != nil {
+		for k, v := range parameters {
+            query.Set(k, v)
+		}
+	}
+
+    url.RawQuery = query.Encode()
+
+    fmt.Println(url.String())
+
+    req, _ := http.NewRequest("GET", url.String(), nil)
+    req.Header.Add("X-API-KEY", c.apiKey)
+    req.Header.Add("Client", "Go SDK")
+
+    response, doError := httpClient.Do(req)
+	if doError != nil {
+		return nil, errors.New("Cannot create request")
+	}
+
+    defer response.Body.Close()
+
+    decoder := json.NewDecoder(response.Body)
+	var result Result
+
+	decodeError := decoder.Decode(&result)
+	if decodeError != nil {
+		return nil, errors.New("cannot decode")
+	}
+
+    return result, nil
+}
+
+func (c Client) GetRequestArchive(requestId string) (Result, error) {
+    return c.getAPIRequest("/requests/"+ requestId, map[string]string {})
+}
+
+func (c Client) GoogleSearch(parameters map[string]string) ([]interface{}, error) {
+    parameters["async"] = "false"
+    response, err := c.getAPIRequest("/google-search-v2", parameters)
+    return response["data"].([]interface{}), err
+}
+
+func (c Client) GoogleSearchV3(parameters map[string]string) ([]interface{}, error) {
+    parameters["async"] = "false"
+    response, err := c.getAPIRequest("/google-search-v3", parameters)
+    return response["data"].([]interface{}), err
+}
+
+func (c Client) GoogleMapsSearchV2(parameters map[string]string) ([]interface{}, error) {
+    parameters["async"] = "false"
+    response, err := c.getAPIRequest("/maps/search-v2", parameters)
+    return response["data"].([]interface{}), err
+}
+
+func (c Client) GoogleMapsReviewsV3(parameters map[string]string) ([]interface{}, error) {
+    parameters["async"] = "false"
+    response, err := c.getAPIRequest("/maps/reviews-v3", parameters)
+    return response["data"].([]interface{}), err
+}
+
+func (c Client) EmailsAndContacts(parameters map[string]string) ([]interface{}, error) {
+    parameters["async"] = "false"
+    response, err := c.getAPIRequest("/emails-and-contacts", parameters)
+    return response["data"].([]interface{}), err
+}
+
+func (c Client) PhonesEnricher(parameters map[string]string) ([]interface{}, error) {
+    parameters["async"] = "false"
+    response, err := c.getAPIRequest("/phones-enricher", parameters)
+    return response["data"].([]interface{}), err
 }
